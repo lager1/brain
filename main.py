683c206850d7a024f086d64693c9873b40ab6298
Václav Mach <vac.mach@sh.cvut.cz>
2013-04-07 16:01:30 +0200

    uvodni commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+
+import os.path
+
+def main():
+  if os.path.isfile(argv[0]):
+    echo("je soubor")
+  else:
+    echo("neni soubor")
+
+
+
+
+main
+
+
