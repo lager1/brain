3059e55ba5f1e72ee6c41134255ea0ecde1d07be
Václav Mach <vac.mach@sh.cvut.cz>
2013-04-30 21:14:30 +0200

    pridano cteni vstupu

--- brainx.py
+++ brainx.py
@@ -1,7 +1,6 @@
 ﻿#!/opt/python3.3/bin/python3.3
 # -*- coding: utf-8 -*-
 
-
 # ------------------------------------------------------------------------------
 # import knihoven
 # ------------------------------------------------------------------------------
@@ -13,27 +12,33 @@ import struct
 class BrainFuck:
   """Interpretr jazyka brainfuck."""
 
-  #def __init__(self, data, memory = b'\x00', memory_pointer = 0):
-  def __init__(self, data, memory = bytearray(b'\x00'), memory_pointer = 0):
+  def __init__(self, data, memory = b'\x00', memory_pointer = 0):
     """Inicializace interpretru brainfucku."""
 
     # data programu
     self.data = data
 
     # inicializace proměnných
-    self.memory = memory
+    self.memory = bytearray(memory)
+    # zde priradit bytearray misto v hlavicce
     self.memory_pointer = memory_pointer
-
 
     # DEBUG a testy
     # a) paměť výstupu
     self.output = ''
 
     self.dataCheck()
-    #self.loop(0, len(self.data))
-    self.loop(0, len(self.data) - 1)
+    self.loop(0, len(self.data))
 
   # pridat funkci pro ocisteni kodu od komentaru ?
+  # -> urcite ma smysl ==> dobra optimalizace, protoze se musime vracet v kodu
+
+
+  # dale ma smysl optimalizovat pomci spojovani stejnych prikazu zasebou -> napr 30 x + muzu vykonat jako jeden prikaz a nemusis 30 x iterovat
+
+  # odevzdani v podstate do konce semestru
+
+
   # pridat odchyceni vyjimky bytearray index out of range ?
   # --> pridat kontrolu preteceni a podteceni
 
@@ -60,6 +65,8 @@ class BrainFuck:
         if i not in "<>+-.,[]!":     # kontrola zda se sklada pouze z dovolenych znaku
           sys.exit("zadany soubor \"" + self.data + "\" neexistuje")
 
+          # tady by to chtelo jeste predelat -> kod muze obsahovat i komentare !!!
+
     elif not os.path.isfile(self.data):
       sys.exit("zadany soubor \"" + self.data + "\" neni soubor")
 # ------------------------------------------------------------------------------
@@ -81,6 +88,8 @@ class BrainFuck:
       elif self.data[i] == '<':
         self.memory_pointer -= 1
 
+        # tadyo dodelat kontrolu ze nelze jit doleva, pokud jsme na zacatku pasky
+
       elif self.data[i] == '+':
         self.memory[self.memory_pointer] += 1   # inkrementace aktualni pametove bunky
 
@@ -90,10 +99,10 @@ class BrainFuck:
       elif self.data[i] == '.':
         self.output = self.memory.decode("utf-8")[self.memory_pointer]
         print(self.output, end="")
+        sys.stdout.flush()
 
       elif self.data[i] == ',':
-        pass
-        # cteni vstupu
+        self.memory[self.memory_pointer] = ord(sys.stdin.read(1))
 
       elif self.data[i] == '[':
 
@@ -133,7 +142,7 @@ class BrainLoller():
     # ..který pak předhodíme interpretru
     self.program = BrainFuck(self.data)
 
-
+# ------------------------------------------------------------------------------
 class BrainCopter():
   """Třída pro zpracování jazyka braincopter."""
 
