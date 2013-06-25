aabd51f48dde2a139a4fd82879cd11412aa65bda
lager1 <13869941+lager1@users.noreply.github.com>
2013-06-25 12:44:26 +0200

    added CRC check

--- image_png.py
+++ image_png.py
@@ -23,6 +23,16 @@ class PNGNotImplementedError(Exception):
       return repr(self.value)
 
 
+class PNGWrongCrcError(Exception):
+    """Výjimka oznamující, že PNG-obrázek je poškozen."""
+
+    def __init__(self, value):
+      self.value = value
+
+    def __str__(self):
+      return repr(self.value)
+
+
 class PngReader():
     """Třída pro práci s PNG-obrázky."""
 
@@ -40,122 +50,90 @@ class PngReader():
         self.ihdr_type = f.read(4)   # typ chunku
 
         if self.ihdr_type != b'IHDR':
-          raise PNGWrongHeaderError("Strukturu zadaného souboru není možné zpracovat.")
+          raise PNGNotImplementedError("Strukturu zadaného souboru není možné zpracovat.")
 
-        self.ihdr_width = f.read(4)  # vyska obrazku v pixelech
-        self.ihdr_height = f.read(4) # sirka obrazku v pixelech
-        self.ihdr_depth = f.read(1)  # bitova hloubka
-        self.ihdr_col_type = f.read(1)   # barvovy typ
-        self.ihdr_compression = f.read(1)   # metoda komprese
-        self.ihdr_filter = f.read(1)        # metoda filtrace
-        self.ihdr_interlace = f.read(1)     # metoda prokladani
+        self.ihdr_data = f.read(13)  # 13 bajtu dat
+
+        self.ihdr_width = self.ihdr_data[:4]  # vyska obrazku v pixelech
+        self.ihdr_height = self.ihdr_data[4:8] # sirka obrazku v pixelech
+        self.ihdr_depth = self.ihdr_data[8:9]  # bitova hloubka
+        self.ihdr_col_type = self.ihdr_data[9:10]   # barvovy typ
+        self.ihdr_compression = self.ihdr_data[10:11]   # metoda komprese
+        self.ihdr_filter = self.ihdr_data[11:12]        # metoda filtrace
+        self.ihdr_interlace = self.ihdr_data[12:13]     # metoda prokladani
+
         self.ihdr_crc = f.read(4)     # kontrolni soucet
 
-
-        #print(self.ihdr_len)
-        #print(struct.unpack('>cccc', self.ihdr_len))
-        #print("------------------------------------------------------")
+        if zlib.crc32(self.ihdr_type + self.ihdr_data) != struct.unpack('>I', self.ihdr_crc)[0]:
+          raise PNGWrongCrcError("Zadaný soubor je pravděpodobně poškozen.")
 
         self.chunk_len = f.read(4)
-        while chunk_len != b'\x00\x00\x00\x00':     # cteni vseho az do IEND
+        while self.chunk_len != b'\x00\x00\x00\x00':     # cteni vseho az do IEND
 
           self.chunk_type = f.read(4)    # typ chunku
 
           if self.chunk_type != b'IDAT':
-            raise PNGWrongHeaderError("Strukturu zadaného souboru není možné zpracovat.")
+            raise PNGNotImplementedError("Strukturu zadaného souboru není možné zpracovat.")
 
-          self.chunk_len_int = 0
           self.idat = b''
 
-          #print(self.chunk_len)
-          #print(int(chunk_len, 16))
-          #print(int(struct.unpack('>cccc', chunk_len), 16))
-
-
-          # toto chce udelat nejak lepe, nebude to fungovat vzdy
-          for i, j in enumerate(struct.unpack('>cccc', self.chunk_len)):
-            #print(i)
-            #print(j)
-            #print(int(str(j).strip('b\'').strip('\'')))
-            #print(str(j).strip('b\'\\x').strip('\''))
-            #print("delka: ")
-            #print(int(str(j).strip('b\'\\x').strip('\''), 16))
-            #print(255**(4 - i))
-            #print(int(str(j).strip('b\'\\x').strip('\''), 16) * 255**(3 - i))
-            self.chunk_len_int += int(str(j).strip('b\'\\x').strip('\''), 16) * 256**(3 - i)
-
-
-
-            #print(int(j.decode(), 16))
-            #print(eval(j))
-            #print(j.decode("ascii"))
-
-          #print(struct.unpack(chunk_len)
-
-
-
-          self.chunk_data = f.read(self.chunk_len_int)
+          self.chunk_data = f.read(struct.unpack('>I', self.chunk_len)[0])  # precteni vsech dat chunku
           self.idat += self.chunk_data
           self.chunk_crc = f.read(4)
 
-
-          print(self.chunk_len_int)
-          print(self.chunk_type)
-          print(self.chunk_data)
-          print(self.chunk_crc)
+          if zlib.crc32(self.chunk_type + self.chunk_data) != struct.unpack('>I', self.chunk_crc)[0]:
+            raise PNGWrongCrcError("Zadaný soubor je pravděpodobně poškozen.")
 
           self.chunk_len = f.read(4)
 
-          #chunk_data = 
-
-
-        self.iend_len = f.read(4)    # delka
+        self.iend_len = self.chunk_len
         self.iend_type = f.read(4)   # typ
         self.iend_crc = f.read(4)   # crc
 
-        print("------------------------------------------------------")
+        if zlib.crc32(self.iend_type) != struct.unpack('>I', self.iend_crc)[0]:
+          raise PNGWrongCrcError("Zadaný soubor je pravděpodobně poškozen.")
 
-        print(self.iend_len)
-        print(self.iend_type)
-        print(self.iend_crc)
-
-        rest = f.read()
-
-        print(rest)
         print("------------------------------------------------------")
         #print(zlib.decompress(self.chunk_data))
         print(zlib.decompress(self.idat))
 
 
-
         print("------------------------------------------------------")
 
 
 
-        self.data = str(zlib.decompress(self.idat)).strip('b\'\\x').strip('\'').split('\\x')
+        #self.data = str(zlib.decompress(self.idat)).strip('b\'\\x').strip('\'').split('\\x')
+        self.data = zlib.decompress(self.idat)
+        print(self.data)
+
+
+        print(struct.unpack('>I', self.ihdr_width)[0])
+        print(struct.unpack('>I', self.ihdr_height)[0])
+        print(struct.unpack('>B', self.ihdr_depth)[0])
+
         self.data_int = []
 
-        for i in self.data:
-          self.data_int.append(int(i, 16))      # konverze na cisla
+        #for i in self.data:
+        #  self.data_int.append(int(i, 16))      # konverze na cisla
 
 
 
-        #for i, j in enumerate(self.data):
-        #  if i % 10 == 0:
-        #    self.rgb += self.data[i + 1:]
-        #  print(int(j, 16), i)
-        #  #print(i)
+        ##for i, j in enumerate(self.data):
+        ##  if i % 10 == 0:
+        ##    self.rgb += self.data[i + 1:]
+        ##  print(int(j, 16), i)
+        ##  #print(i)
 
-        for i in range(0, len(self.data_int), 10):
-          tmp = []
-          for j in range(1, 10, 3):
-            #tmp.append(list(self.data_int[i + j:i + j + 3]))
-            tmp.append(tuple(self.data_int[i + j:i + j + 3]))
+        #for i in range(0, len(self.data_int), 10):
+        #  tmp = []
+        #  for j in range(1, 10, 3):
+        #    #tmp.append(list(self.data_int[i + j:i + j + 3]))
+        #    tmp.append(tuple(self.data_int[i + j:i + j + 3]))
 
-          #self.rgb.append(self.data_int[i + 1:i + 10])
-          self.rgb.append(tmp)
+        #  #self.rgb.append(self.data_int[i + 1:i + 10])
+        #  self.rgb.append(tmp)
 
-        print(self.rgb)
+        #print(self.rgb)
 
         #self.rgb = []   # vycisteni, pouze debug
 
